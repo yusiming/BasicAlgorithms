21976795df2b95268acbdf23e531ca98277cded4
虞四明 <13966931523@163.com>
2018-12-14 13:34:42 +0800

    冒泡排序

--- src/yu/sort/BubbleSort.java
+++ src/yu/sort/BubbleSort.java
@@ -7,6 +7,14 @@ package yu.sort;
  * @Date 2018/12/14 11:48
  */
 public class BubbleSort {
+    /**
+     * 第一轮比较：3 2 4 0 7 ==> 2 3 4 0 7 ==> 2 3 0 4 7
+     * 第二轮比较：2 3 0 4 7 ==> 2 0 3 4 7
+     * 第三轮比较：2 0 3 4 7 ==> 0 2 3 4 7
+     * ......
+     *
+     * @param arr 需要排序的数组
+     */
     public static void bubbleSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
