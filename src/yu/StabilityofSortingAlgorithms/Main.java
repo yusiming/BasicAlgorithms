c942c40d0ae96071134797572646fb4a4f4eff3a
虞四明 <13966931523@163.com>
2018-12-22 10:01:58 +0800

    比较器的使用

---
+++ src/yu/StabilityofSortingAlgorithms/Main.java
@@ -0,0 +1,22 @@
+package yu.StabilityofSortingAlgorithms;
+
+import java.util.PriorityQueue;
+
+/**
+ * @Auther yusiming
+ * @Date 2018/12/21 10:26
+ */
+public class Main {
+    public static void main(String[] args) {
+        PriorityQueue<Student> priorityQueue = new PriorityQueue<>((Student a, Student b) -> a.getId() - b.getId());
+        Student a = new Student("yu", 1, 20);
+        Student b = new Student("si", 2, 30);
+        Student c = new Student("ming", 3, 25);
+        priorityQueue.add(a);
+        priorityQueue.add(b);
+        priorityQueue.add(c);
+        while (!priorityQueue.isEmpty()) {
+            System.out.println(priorityQueue.poll());
+        }
+    }
+}
---
+++ src/yu/StabilityofSortingAlgorithms/Student.java
@@ -0,0 +1,53 @@
+package yu.StabilityofSortingAlgorithms;
+
+/**
+ * @Auther yusiming
+ * @Date 2018/12/21 10:22
+ */
+public class Student {
+    private String name;
+    private int id;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", age=" + age +
+                '}';
+    }
+}
