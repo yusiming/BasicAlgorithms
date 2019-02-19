5e55c6602de597bd2f58d27103c9fadc9bd6b83d
虞四明 <13966931523@163.com>
2019-02-19 11:41:14 +0800

    宠物类

---
+++ src/yu/CatDogQueue/Pet.java
@@ -0,0 +1,19 @@
+package yu.CatDogQueue;
+
+/**
+ * 宠物类
+ *
+ * @Auther yusiming
+ * @Date 2019/2/19 11:21
+ */
+public class Pet {
+    private String type;
+
+    public Pet(String type) {
+        this.type = type;
+    }
+
+    public String getPetType() {
+        return this.type;
+    }
+}
