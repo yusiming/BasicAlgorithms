898b9d3d5ce3169f829fbeab10db2b40a68bbe15
虞四明 <13966931523@163.com>
2019-02-21 12:33:01 +0800

    二叉树的前、中、后序遍历的递归与非递归实现

---
+++ src/yu/binaryTree/PrintBinaryTree.java
@@ -0,0 +1,144 @@
+package yu.binaryTree;
+
+import java.util.Stack;
+
+/**
+ * 二叉树的前、中、后序遍历的递归与非递归实现
+ *
+ * @Auther yusiming
+ * @Date 2019/2/21 11:27
+ */
+public class PrintBinaryTree {
+    private static class Node {
+        Node left;
+        Node right;
+        int value;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    /**
+     * 前序遍历的递归实现
+     */
+    public static void preOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.value + " ");
+        preOrderRecur(head.left);
+        preOrderRecur(head.right);
+    }
+
+    /**
+     * 中序遍历的递归实现
+     */
+    public static void inOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        inOrderRecur(head.left);
+        System.out.print(head.value + " ");
+        inOrderRecur(head.right);
+    }
+
+    /**
+     * 后序遍历的递归实现
+     */
+    public static void posOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        posOrderRecur(head.left);
+        posOrderRecur(head.right);
+        System.out.print(head.value + " ");
+    }
+
+    /**
+     * 前序遍历的非递归方式
+     */
+    public static void preOrderUnRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        stack.push(head);
+        while (!stack.isEmpty()) {
+            head = stack.pop();
+            System.out.print(head.value + " ");
+            if (head.right != null) {
+                stack.push(head.right);
+            }
+            if (head.left != null) {
+                stack.push(head.left);
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     * 中序遍历的非递归方式
+     */
+    public static void inOrderUnRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        while (!stack.isEmpty() || head != null) {
+            if (head != null) {
+                stack.push(head);
+                head = head.left;
+            } else {
+                head = stack.pop();
+                System.out.print(head.value + " ");
+                head = head.right;
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     * 后序遍历的非递归方式
+     */
+    public static void posOrderUnRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        stack1.push(head);
+        while (!stack1.isEmpty()) {
+            head = stack1.pop();
+            // System.out.print(head + " ");
+            stack2.push(head.value);
+            if (head.left != null) {
+                stack1.push(head.left);
+            }
+            if (head.right != null) {
+                stack1.push(head.right);
+            }
+        }
+        while (!stack2.isEmpty()) {
+            System.out.print(stack2.pop() + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(5);
+        head.left = new Node(3);
+        head.right = new Node(8);
+        head.left.left = new Node(2);
+        head.left.right = new Node(4);
+        head.left.left.left = new Node(1);
+        head.right.left = new Node(7);
+        head.right.left.left = new Node(6);
+        head.right.right = new Node(10);
+        head.right.right.left = new Node(9);
+        head.right.right.right = new Node(11);
+
+        PrintBinaryTree.preOrderUnRecur(head);
+        PrintBinaryTree.inOrderUnRecur(head);
+        PrintBinaryTree.posOrderUnRecur(head);
+    }
+}
